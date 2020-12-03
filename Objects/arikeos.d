222a5fe0b35f1c4151f60c76450f88aca1003bde
Phil <phillip.oni.b@gmail.com>
2020-12-03 00:54:08 -0500

    Removed auto generated files and dirs

--- Objects/arikeos.d
+++
@@ -1,6 +0,0 @@
-.\objects\arikeos.o: arikeos.cpp
-.\objects\arikeos.o: arikeos.h
-.\objects\arikeos.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\cstdint
-.\objects\arikeos.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\arikeos.o: mcal_reg.h
-.\objects\arikeos.o: reg_access.h
--- Objects/main.d
+++
@@ -1,4 +0,0 @@
-.\objects\main.o: main.cpp
-.\objects\main.o: arikeos.h
-.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\cstdint
-.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h

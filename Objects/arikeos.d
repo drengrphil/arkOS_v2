30cc81f587efc8613f14edbd0239b75049b9737c
Phil <phillip.oni.b@gmail.com>
2020-12-03 00:29:51 -0500

    first commit

---
+++ Objects/arikeos.d
@@ -0,0 +1,6 @@
+.\objects\arikeos.o: arikeos.cpp
+.\objects\arikeos.o: arikeos.h
+.\objects\arikeos.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\cstdint
+.\objects\arikeos.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\arikeos.o: mcal_reg.h
+.\objects\arikeos.o: reg_access.h
---
+++ Objects/arikeosa.d
@@ -0,0 +1,1 @@
+.\objects\arikeosa.o: arikeosa.s
